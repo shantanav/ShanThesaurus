c0305061a05fdda1eb56981fb804b1b5a8132b0a
Shantanav Saurav <ss9415@rit.edu>
2021-07-04 02:12:23 +0200

    made a simple navbar to screw around with Vue

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-screen fixed bg-gray-900 text-gray-50 shadow-inner">
+    <div class="float-left">
+      <a class="md:text-xl inline-block p-4 transition duration-500 hover:bg-gray-50 hover:text-gray-900" href="#">Shanthesaurus</a>
+    </div>
+    <div class="float-right">
+      <a class="md:text-xl inline-block p-4 transition duration-500 hover:bg-gray-50 hover:text-gray-900">Submit An Entry</a>
+      <a class="md:text-xl md:inline-block p-4 hidden transition duration-500 hover:bg-gray-50 hover:text-gray-900" href="https://github.com/shantanav/ShanThesaurus" target="_blank">View On GitHub</a>
+      <a class="md:text-xl md:inline-block p-4 hidden transition duration-500 hover:bg-gray-50 hover:text-gray-900">Sign In</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
